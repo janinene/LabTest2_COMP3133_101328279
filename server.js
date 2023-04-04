5a3e70b9b37a0e99a1a4863de0020891f609f503
janinene <95052472+janinene@users.noreply.github.com>
2023-04-04 00:00:14 -0400

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/labtest2-101328279'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/labtest2-101328279/index.html'));});
-app.listen(process.env.PORT || 8080);
